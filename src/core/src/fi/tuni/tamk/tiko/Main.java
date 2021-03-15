137ddd42081a4286866fa99e29b199732dede7b4
Ville Karilainen <ville.karilainen@gmail.com>
2021-03-15 22:17:49 +0200

    added primal play button

--- src/core/src/fi/tuni/tamk/tiko/Main.java
+++ src/core/src/fi/tuni/tamk/tiko/Main.java
@@ -3,20 +3,35 @@ package fi.tuni.tamk.tiko;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.Game;
 
+import java.awt.Rectangle;
+
 public class Main extends Game {
+
+	Sprite playButton;
+	Texture playButtonTexture;
 	SpriteBatch batch;
 	OrthographicCamera camera;
 	Texture backgroundImage;
-
+	float playButtonX;
+	float playButtonY;
+	float playButtonWidth;
+	float playButtonHeight;
 	public SpriteBatch getBatch() {
 		return batch;
 	}
 	Texture img;
 	@Override
 	public void create () {
+		playButtonX = 250;
+		playButtonY = 600-playButtonHeight;
+		playButtonWidth = 200;
+		playButtonHeight = 100;
+
+		playButtonTexture = new Texture("default.png");
 		batch = new SpriteBatch();
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 9,16);
--- src/core/src/fi/tuni/tamk/tiko/MainMenu.java
+++ src/core/src/fi/tuni/tamk/tiko/MainMenu.java
@@ -5,18 +5,41 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+
+import java.awt.Rectangle;
 
 public class MainMenu implements Screen {
-
-    Main host;
+    Sprite playButton;
     SpriteBatch batch;
     OrthographicCamera camera;
+    Texture backgroundImage;
+    float playButtonX;
+    float playButtonY;
+    float playButtonWidth;
+    float playButtonHeight;
+
+    Main host;
+   // SpriteBatch batch;
+   // OrthographicCamera camera;
 
     public MainMenu(Main host) {
         this.host = host;
         batch = host.batch;
         camera = host.camera;
+        //button = host.button;
+        //buttonTexture = host.buttonTexture;
+        playButtonWidth = host.playButtonWidth;
+        playButtonHeight = host.playButtonHeight;
+        playButton = new Sprite(new Texture("default.png"));
+        playButton.setSize(playButtonWidth, playButtonHeight);
+        playButton.setX(host.playButtonX);
+        playButton.setY(host.playButtonY);
+
+
+
     }
 
 
@@ -31,9 +54,20 @@ public class MainMenu implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         //draws the background
-
+        playButton.draw(batch);
         if (Gdx.input.isTouched()){
-            host.setScreen(new CoreGameplayLoop(host));
+            int realX = Gdx.input.getX();
+            int realY = Gdx.input.getY();
+            Vector3 touchPos = new Vector3(realX, realY,0);
+            camera.unproject(touchPos);
+            System.out.println(touchPos.x);
+            System.out.println(touchPos.y);
+            if (touchPos.x > playButton.getX() && touchPos.x > playButton.getX() + playButton.getWidth()
+                    &&  touchPos.y > playButton.getY() && touchPos.y > playButton.getY() + playButton.getHeight()) {
+
+                          host.setScreen(new CoreGameplayLoop(host));
+
+            }
         }
         batch.end();
     }
