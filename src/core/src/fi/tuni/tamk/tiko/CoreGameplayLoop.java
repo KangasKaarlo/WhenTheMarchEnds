239f6d9e2176371fe58fb6240858573c0d24eb32
Kaarlo Kangas <kaarlo.kangas@gmail.com>
2021-03-15 16:37:33 +0200

    Changed the background color

--- src/core/src/fi/tuni/tamk/tiko/CoreGameplayLoop.java
+++ src/core/src/fi/tuni/tamk/tiko/CoreGameplayLoop.java
@@ -108,7 +108,7 @@ public class CoreGameplayLoop implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 1, 0, 1);
+        Gdx.gl.glClearColor(0.3f, 0.5f, 0.67f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         updateAttributes();
