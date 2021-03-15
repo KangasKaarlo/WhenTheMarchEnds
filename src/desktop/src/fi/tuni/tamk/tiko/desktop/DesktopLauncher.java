08e7366006a0a94c006500d4df2b45f1f58b3133
Kaarlo Kangas <kaarlo.kangas@gmail.com>
2021-03-15 13:33:25 +0200

    Configured DesktopLauncher

--- src/desktop/src/fi/tuni/tamk/tiko/desktop/DesktopLauncher.java
+++ src/desktop/src/fi/tuni/tamk/tiko/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import fi.tuni.tamk.tiko.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 675;
+		config.height = 1200;
 		new LwjglApplication(new Main(), config);
 	}
 }
